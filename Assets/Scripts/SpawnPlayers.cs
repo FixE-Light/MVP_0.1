8e5874ce262272c702965a524f132b2ccaa1352d
Mitrandir-main <dian.kumanov@yahoo.com>
2022-04-04 13:41:16 +0100

    [add] working multiplayer with firebase auth

--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -9,16 +9,16 @@ public class SpawnPlayers : MonoBehaviour
 {
     public GameObject player;
 
-    [SerializeField]
-    public FirebaseManager firebaseManager = null;
+    // [SerializeField]
+    // public FirebaseManager firebaseManager = null;
 
-    [SerializeField]
-    private TMP_Text Username;
+    // [SerializeField]
+    // private TMP_Text Username;
 
     public float minX, minY, maxX, maxY;
     public void Start()
     {
-        Username.text = firebaseManager.user.DisplayName;
+        // Username.text = firebaseManager.user.DisplayName;
         Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
         PhotonNetwork.Instantiate(player.name, randomPosition, Quaternion.identity);
     }
